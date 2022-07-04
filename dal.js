ef4d83b0390f7515557478376b9242797d9e5db3
Kyra Hays <kyra101w@gmail.com>
2022-07-04 17:05:23 -0500

    initial commit

---
+++ dal.js
@@ -0,0 +1,81 @@
+const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const uri         = process.env.MONGODB_CONNECTION_STRING;
+let db            = null;
+ 
+// connect to mongo
+MongoClient.connect(uri, {useUnifiedTopology: true}, function(err, client) {
+    console.log("Connected successfully to db server");
+
+    console.log(process.env.MONGODB_STRING) 
+    if (err) console.log(err);
+    // connect to myproject database
+    db = client.db("badbank");
+});
+
+// create user account
+function create(name, email, password){
+    return new Promise((resolve, reject) => {    
+        const collection = db.collection('customers');
+        const doc = {name, email, password, balance: 0};
+        collection.insertOne(doc, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });    
+    })
+}
+
+// find user account
+function find(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('customers')
+            .find({email: email})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// find user account
+function findOne(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('customers')
+            .findOne({email: email})
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));    
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('customers')            
+            .findOneAndUpdate(
+                {email: email},
+                { $inc: { balance: amount}},
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );            
+
+
+    });    
+}
+
+// all users
+function all(){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('customers')
+            .find({})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+
+module.exports = {create, findOne, find, update, all};
---
+++ public/home.js
@@ -0,0 +1,12 @@
+function Home(){
+
+  return (
+    <Card
+      txtcolor="black"
+      header="Not as Bad Bank"
+      title="Welcome to the bad bank that isn't THAT bad"
+      text="Login or create an account to begin adding 'funds'"
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />
+  );  
+}
